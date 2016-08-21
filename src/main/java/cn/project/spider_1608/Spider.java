aca26738f62ca48e0a5ba02dd0c68f9ce7e14ae3
StephenLU0422 <1029252135@qq.com>
2016-08-22 01:35:48 +0800

    simple method

--- src/main/java/cn/project/spider_1608/Spider.java
+++ src/main/java/cn/project/spider_1608/Spider.java
@@ -5,5 +5,24 @@ public class Spider {
 	public void start() {
 		
 	}
+	/**
+	 * 下载页面数据
+	 * @param url
+	 */
+	public void download(String url) {
+		
+	}
+	/**
+	 * 解析页面数据
+	 */
+	public void process() {
+		
+	}
+	/**
+	 * 存储页面
+	 */
+	public void store() {
+		
+	}
 
 }
