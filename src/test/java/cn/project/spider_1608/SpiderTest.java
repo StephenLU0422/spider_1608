19a57bd23e0239a4b0d900a9a37d9c256b86f652
StephenLU0422 <1029252135@qq.com>
2016-08-22 01:03:35 +0800

    Spider test

---
+++ src/test/java/cn/project/spider_1608/SpiderTest.java
@@ -0,0 +1,14 @@
+package cn.project.spider_1608;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SpiderTest {
+	@Test
+	public void test() throws Exception {
+		//假设有spider
+		Spider spider = new Spider();
+		spider.start();
+	}
+}
