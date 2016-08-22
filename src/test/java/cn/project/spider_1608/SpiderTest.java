5fc11d6f473d58aa27016fd9eddb8df5f2ad3b85
StephenLU0422 <1029252135@qq.com>
2016-08-22 11:04:02 +0800

    SpiderTest inject Downloadable impl

--- src/test/java/cn/project/spider_1608/SpiderTest.java
+++ src/test/java/cn/project/spider_1608/SpiderTest.java
@@ -4,15 +4,20 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import cn.project.spider_1608.domain.Page;
+import cn.project.spider_1608.download.HttpClientDownload;
+
 public class SpiderTest {
 	@Test
 	public void test() throws Exception {
 		//假设有spider
 		Spider spider = new Spider();
 //		spider.start();
+		spider.setDownloadable(new HttpClientDownload());
 		String url ="http://item.jd.com/2569127.html";
-		spider.download(url);
-		spider.process();
-		spider.store();
+		Page page = spider.download(url);
+		System.out.println(page.getContent());
+		spider.process(page);
+		spider.store(page);
 	}
 }
