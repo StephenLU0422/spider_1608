12d49c74ad9818e8e1501b624f922218d1f9e3b4
StephenLU0422 <1029252135@qq.com>
2016-08-22 10:22:24 +0800

    retuen Page object

---
+++ src/main/java/cn/project/spider_1608/domain/Page.java
@@ -0,0 +1,16 @@
+package cn.project.spider_1608.domain;
+
+public class Page {
+	/**
+	 * 原始页面内容
+	 */
+	private String content;
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
