c09df723754a066068edc0b0917a5ec4bd5b597a
StephenLU0422 <1029252135@qq.com>
2016-08-22 22:12:50 +0800

    HtmlUtil have getText(rootNode,xpath),getAttributeByName(rootNode,attr,xpath)

---
+++ src/main/java/cn/project/spider_1608/utils/HtmlUtils.java
@@ -0,0 +1,48 @@
+package cn.project.spider_1608.utils;
+
+import org.htmlcleaner.TagNode;
+import org.htmlcleaner.XPatherException;
+
+public class HtmlUtils {
+	/**
+	 * 根据xpath获取指定标签的内容
+	 * @param rootNode
+	 * @param xpath
+	 * @return
+	 */
+	public static String getText(TagNode tagNode,String xpath){
+		
+		//title
+		String value ="";
+		Object[] evaluateXPath;
+		try {
+			evaluateXPath = tagNode.evaluateXPath("//div[@class='sku-name']");
+			if (evaluateXPath.length>0) {
+				TagNode titleNode = (TagNode)evaluateXPath[0];
+				//获取里面的内容
+				//page.addFiled("title", titleNode.getText().toString());
+				value= titleNode.getText().toString();
+			}
+		} catch (XPatherException e) {
+			e.printStackTrace();
+		}
+		return value;
+	}
+	
+	public static String getAttributeByName(TagNode tagNode,String attr,String xpath){
+		String value = "";
+		Object[] nodeObjs;
+		try {
+			nodeObjs = tagNode.evaluateXPath(xpath);
+			if (nodeObjs!=null&& nodeObjs.length>0) {
+				TagNode node = (TagNode)nodeObjs[0];
+				value =node.getAttributeByName(attr);
+			}
+		} catch (XPatherException e) {
+			e.printStackTrace();
+		}
+		return value;
+	}
+	
+	
+}
