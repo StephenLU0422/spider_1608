b90fde6029b249b43b42ce48ad7b53411562cb6f
StephenLU0422 <1029252135@qq.com>
2016-08-22 10:06:15 +0800

    Impl http client download method

--- src/main/java/cn/project/spider_1608/Spider.java
+++ src/main/java/cn/project/spider_1608/Spider.java
@@ -1,5 +1,18 @@
 package cn.project.spider_1608;
 
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 public class Spider {
 
 	public void start() {
@@ -10,6 +23,22 @@ public class Spider {
 	 * @param url
 	 */
 	public void download(String url) {
+		//获取httpclient
+		HttpClientBuilder builder = HttpClients.custom();
+		CloseableHttpClient client = builder.build();
+		
+		HttpGet request = new HttpGet(url);
+		try {
+			CloseableHttpResponse response = client.execute(request);
+			//获取页面内容
+			HttpEntity entity = response.getEntity();
+			System.out.println(EntityUtils.toString(entity));
+			
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	/**
