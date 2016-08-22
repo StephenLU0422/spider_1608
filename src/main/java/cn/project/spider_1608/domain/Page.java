c2a6338b9a1b40ed91c18657c7b60b25f672b689
StephenLU0422 <1029252135@qq.com>
2016-08-22 20:15:41 +0800

    Page add product message商品信息 HashMap

--- src/main/java/cn/project/spider_1608/domain/Page.java
+++ src/main/java/cn/project/spider_1608/domain/Page.java
@@ -1,16 +1,44 @@
 package cn.project.spider_1608.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Page {
+	/**
+	 * 原始url
+	 */
+	private String url;
 	/**
 	 * 原始页面内容
 	 */
 	private String content;
-
+	
+	/**
+	 * 存储商品基本参数信息
+	 */
+	private Map<String,String> map = new HashMap<String,String>();
+	
 	public String getContent() {
 		return content;
 	}
 
 	public void setContent(String content) {
 		this.content = content;
+	}
+	//url get/set method
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	//map
+	public Map<String, String> getMap() {
+		return map;
+	}
+
+	public void addFiled(String key,String value) {
+		this.map.put(key, value);
 	}
 }
