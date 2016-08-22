2e16ce2b4b214ee58174e7bdad882a2c3e846b55
StephenLU0422 <1029252135@qq.com>
2016-08-22 23:01:28 +0800

    HbaseStoreImpl store(Page) store

---
+++ src/main/java/cn/project/spider_1608/store/HbaseStoreImpl.java
@@ -0,0 +1,12 @@
+package cn.project.spider_1608.store;
+
+import cn.project.spider_1608.domain.Page;
+
+public class HbaseStoreImpl implements Storeable {
+
+	@Override
+	public void store(Page page) {
+
+	}
+
+}
