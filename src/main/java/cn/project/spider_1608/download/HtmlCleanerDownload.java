fe48671f6fca89666a9a2e1af47ea6a7c9216df5
StephenLU0422 <1029252135@qq.com>
2016-08-22 11:07:22 +0800

    HtmlCleanerDownload may have another func

---
+++ src/main/java/cn/project/spider_1608/download/HtmlCleanerDownload.java
@@ -0,0 +1,13 @@
+package cn.project.spider_1608.download;
+
+import cn.project.spider_1608.domain.Page;
+
+public class HtmlCleanerDownload implements Downloadable {
+
+	@Override
+	public Page download(String url) {
+		//impl another download func
+		return null;
+	}
+
+}
