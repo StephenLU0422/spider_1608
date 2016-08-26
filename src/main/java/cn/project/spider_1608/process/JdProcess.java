84c258f5589e8d2a147e05d22e202c8dfd8528c2
StephenLU0422 <1029252135@qq.com>
2016-08-26 12:23:42 +0800

    JdProcess refactor the getPrice() getSpec()

--- src/main/java/cn/project/spider_1608/process/JdProcess.java
+++ src/main/java/cn/project/spider_1608/process/JdProcess.java
@@ -12,6 +12,7 @@ import org.json.JSONObject;
 import cn.project.spider_1608.domain.Page;
 import cn.project.spider_1608.utils.HtmlUtils;
 import cn.project.spider_1608.utils.PageUtils;
+import cn.project.spider_1608.utils.RevUtils;
 
 public class JdProcess implements Processable {
 
@@ -36,60 +37,13 @@ public class JdProcess implements Processable {
 			page.addField("picpath", "http:"+picpath);
 			
 			//get productID获取商品ID
-			String url = page.getUrl();
-			//pattern正则表达式
-			Pattern pattern = Pattern.compile("http://item.jd.com/([0-9]+).html");
-			Matcher matcher = pattern.matcher(url);
-			String goodsId =null;
-			if(matcher.find()){
-				goodsId = matcher.group(1);
-				System.out.println(goodsId);
-			}
-			
-			//price,需要分析js请求
-			/*Object[] priceObjs = rootNode.evaluateXPath("//span[@class='p-price']/span[2]");
-			if (priceObjs!=null && priceObjs.length>0) {
-				TagNode priceNode = (TagNode)priceObjs[0];
-				System.out.println("price:"+priceNode.getText().toString()+"---");
-			}*/
-			String priceJson = PageUtils.getContent("http://p.3.cn/prices/mgets?skuIds=J_"+goodsId);
-			System.out.println(priceJson);
-			JSONArray jsonArray = new JSONArray(priceJson);
-			JSONObject object = (JSONObject)jsonArray.get(0);
+			JSONObject object = getPrice(page);
 			page.addField("price", object.getString("p"));
 			
 			//System.out.println(object.getString("p"));
 			
 			//规格参数
-			JSONArray specjsonArray = new JSONArray();
-			Object[] itemObjs = rootNode.evaluateXPath("//*[@id=\"detail\"]/div[2]/div[2]/div[2]/div");
-			for (Object itemObject : itemObjs) {
-				TagNode itemNode = (TagNode)itemObject;
-				//获取h3标签"主体"
-				Object[] h3objs = itemNode.evaluateXPath("/h3");
-				if (h3objs!=null && h3objs.length>0) {
-					JSONObject h3jsonObj = new JSONObject();
-					TagNode h3Node = (TagNode)h3objs[0];
-					//System.out.println(h3Node.getText().toString());
-					h3jsonObj.put("name", h3Node.getText().toString());
-					h3jsonObj.put("value", "");
-					specjsonArray.put(h3jsonObj);
-				}
-				//
-				Object[] dtobjs = itemNode.evaluateXPath("/dl/dt");
-				Object[] ddobjs = itemNode.evaluateXPath("/dl/dd");
-				if (dtobjs!=null && dtobjs.length>0 && ddobjs!=null && ddobjs.length>0) {
-					for (int i = 0; i < dtobjs.length; i++) {
-						JSONObject dtddjsonObj = new JSONObject();
-						TagNode dtNode =(TagNode)dtobjs[i];
-						TagNode ddNode =(TagNode)ddobjs[i];
-						dtddjsonObj.put("name", dtNode.getText().toString());
-						dtddjsonObj.put("value", ddNode.getText().toString());
-						specjsonArray.put(dtddjsonObj);
-					}
-				}
-				
-			}
+			JSONArray specjsonArray = getSpec(rootNode);
 			page.addField("spec", specjsonArray.toString());
 			
 			
@@ -97,6 +51,64 @@ public class JdProcess implements Processable {
 			e.printStackTrace();
 		}
 		
+	}
+
+	private JSONArray getSpec(TagNode rootNode) throws XPatherException {
+		JSONArray specjsonArray = new JSONArray();
+		Object[] itemObjs = rootNode.evaluateXPath("//*[@id=\"detail\"]/div[2]/div[2]/div[2]/div");
+		for (Object itemObject : itemObjs) {
+			TagNode itemNode = (TagNode)itemObject;
+			//获取h3标签"主体"
+			Object[] h3objs = itemNode.evaluateXPath("/h3");
+			if (h3objs!=null && h3objs.length>0) {
+				JSONObject h3jsonObj = new JSONObject();
+				TagNode h3Node = (TagNode)h3objs[0];
+				//System.out.println(h3Node.getText().toString());
+				h3jsonObj.put("name", h3Node.getText().toString());
+				h3jsonObj.put("value", "");
+				specjsonArray.put(h3jsonObj);
+			}
+			//
+			Object[] dtobjs = itemNode.evaluateXPath("/dl/dt");
+			Object[] ddobjs = itemNode.evaluateXPath("/dl/dd");
+			if (dtobjs!=null && dtobjs.length>0 && ddobjs!=null && ddobjs.length>0) {
+				for (int i = 0; i < dtobjs.length; i++) {
+					JSONObject dtddjsonObj = new JSONObject();
+					TagNode dtNode =(TagNode)dtobjs[i];
+					TagNode ddNode =(TagNode)ddobjs[i];
+					dtddjsonObj.put("name", dtNode.getText().toString());
+					dtddjsonObj.put("value", ddNode.getText().toString());
+					specjsonArray.put(dtddjsonObj);
+				}
+			}
+			
+		}
+		return specjsonArray;
+	}
+
+	private JSONObject getPrice(Page page) {
+		String url = page.getUrl();
+		//pattern正则表达式
+		Pattern pattern = Pattern.compile("http://item.jd.com/([0-9]+).html");
+		Matcher matcher = pattern.matcher(url);
+		String goodsId =null;
+		if(matcher.find()){
+			goodsId = matcher.group(1);
+			System.out.println(goodsId);
+			page.setGoodsid(RevUtils.reverse(goodsId)+"_jd");
+
+		}
+		//price,需要分析js请求
+		/*Object[] priceObjs = rootNode.evaluateXPath("//span[@class='p-price']/span[2]");
+		if (priceObjs!=null && priceObjs.length>0) {
+			TagNode priceNode = (TagNode)priceObjs[0];
+			System.out.println("price:"+priceNode.getText().toString()+"---");
+		}*/
+		String priceJson = PageUtils.getContent("http://p.3.cn/prices/mgets?skuIds=J_"+goodsId);
+		System.out.println(priceJson);
+		JSONArray jsonArray = new JSONArray(priceJson);
+		JSONObject object = (JSONObject)jsonArray.get(0);
+		return object;
 	}
 
 }
