60f45dafcaed84c4f512b438a1de7cc3b8b7d32c
StephenLU0422 <1029252135@qq.com>
2016-08-27 11:26:36 +0800

    OMG!!Page have a naive bug：It's addUrl(String url),not as addUrl(String urlLIst) addUrl方法里面参数应该是商品url，而不是下一页urlList！！

--- src/main/java/cn/project/spider_1608/domain/Page.java
+++ src/main/java/cn/project/spider_1608/domain/Page.java
@@ -1,9 +1,15 @@
 package cn.project.spider_1608.domain;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Page {
+	/**
+	 * 临时存储下一页及当前商品的url，为存到url仓库做准备
+	 */
+	private List<String> urlList = new ArrayList<String>();
 	/**
 	 * 原始url
 	 */
@@ -54,6 +60,14 @@ public class Page {
 
 	public void setGoodsid(String goodsid) {
 		this.goodsid = goodsid;
+	}
+	//url
+	public List<String> getUrlList() {
+		return urlList;
+	}
+		//自己修改
+	public void addUrl(String url) {
+		this.urlList.add(url);
 	}
 
 	
