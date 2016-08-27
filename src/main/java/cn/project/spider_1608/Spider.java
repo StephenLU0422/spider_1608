6a31c881bf39cc7fb442990dae58d846cd9e37e0
StephenLU0422 <1029252135@qq.com>
2016-08-28 00:18:33 +0800

    Spider catch the url sleep()

--- src/main/java/cn/project/spider_1608/Spider.java
+++ src/main/java/cn/project/spider_1608/Spider.java
@@ -14,9 +14,11 @@ import cn.project.spider_1608.download.HttpClientDownload;
 import cn.project.spider_1608.process.JdProcess;
 import cn.project.spider_1608.process.Processable;
 import cn.project.spider_1608.repository.QueueRepository;
+import cn.project.spider_1608.repository.RedisRepository;
 import cn.project.spider_1608.repository.Repository;
 import cn.project.spider_1608.store.ConsoleStoreableImpl;
 import cn.project.spider_1608.store.Storeable;
+import cn.project.spider_1608.utils.SleepUtils;
 
 
 public class Spider {
@@ -34,18 +36,18 @@ public class Spider {
 	//解析的接口
 	private Processable processable;
 	//存储接口
-	private Storeable storeable;
+	private Storeable storeable = new ConsoleStoreableImpl();
 	private Queue<String> queue =new ConcurrentLinkedDeque<String>();
-	//设置存储仓库的接口
-//	private Repository repository =new QueueRepository();
+	//设置存储仓库的接口QueueRepository
+	private Repository repository = new QueueRepository() ;
 	/**
 	 * 启动爬虫
 	 */
 	public void start() {
 		//爬虫一直运行
 		while(true){
-			String url = queue.poll();
-//			String url =repository.poll();
+//			String url = queue.poll();
+			String url =repository.poll();
 			if(StringUtils.isNotBlank(url)){
 				//下载
 				Page page = this.download(url);
@@ -54,7 +56,8 @@ public class Spider {
 				List<String> urlList = page.getUrlList();
 				for (String nexturl : urlList) {
 //					System.out.println(nexturl);
-					this.queue.add(nexturl);
+//					this.queue.add(nexturl);
+					this.repository.add(nexturl);
 //					System.out.println(nexturl);
 				}
 					//优先 把分类的url抓取下来，再抓取分类里面的商品url
@@ -63,9 +66,10 @@ public class Spider {
 						this.store(page);
 		//					repository.addHight(nexturl);
 					}
+					SleepUtils.sleep(1000);
 				}else{
 					System.out.println("没有url了。。。");
-		//					repository.add(nexturl);
+					SleepUtils.sleep(5000);//5s
 				}
 		}
 		
@@ -134,21 +138,22 @@ public class Spider {
 	}
 	//set入口url
 	public void setSeedUrl(String url) {
-		this.queue.add(url);
+//		this.queue.add(url);
+		this.repository.add(url);
 	}
 	//Repository的get/set
-//	public Repository getRepository() {
-//		return repository;
-//	}
-//	public void setRepository(Repository repository) {
-//		this.repository = repository;
-//	}
+	public Repository getRepository() {
+		return repository;
+	}
+	public void setRepository(Repository repository) {
+		this.repository = repository;
+	}
 //	
 	public static void main(String[] args) {
 		Spider spider = new Spider();
-		spider.setDownloadable(new HttpClientDownload());
+//		spider.setDownloadable(new HttpClientDownload());
 		spider.setProcessable(new JdProcess());
-		spider.setStoreable(new ConsoleStoreableImpl());
+//		spider.setStoreable(new ConsoleStoreableImpl());
 		//"http://list.jd.com/list.html?cat=9987%2C653%2C655&go=0"http://item.jd.com/1861098.html
 		String url ="http://list.jd.com/list.html?cat=9987,653,655";
 		spider.setSeedUrl(url);
