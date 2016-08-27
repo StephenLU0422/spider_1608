ecfa279f3fa59d642ebc16ffb2185397292e05d0
StephenLU0422 <1029252135@qq.com>
2016-08-27 15:38:36 +0800

    RedisRepository

---
+++ src/main/java/cn/project/spider_1608/repository/RedisRepository.java
@@ -0,0 +1,20 @@
+package cn.project.spider_1608.repository;
+
+public class RedisRepository implements Repository {
+
+	@Override
+	public String poll() {
+		return null;
+	}
+
+	@Override
+	public void add(String nexturl) {
+
+	}
+
+	@Override
+	public void addHight(String nexturl) {
+
+	}
+
+}
