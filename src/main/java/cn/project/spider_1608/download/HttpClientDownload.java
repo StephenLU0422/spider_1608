cc4c23c3d5d22b62dc8c8bc233f9fa349a9b0738
StephenLU0422 <1029252135@qq.com>
2016-08-27 11:32:38 +0800

    HttpclientDownload no problem!

--- src/main/java/cn/project/spider_1608/download/HttpClientDownload.java
+++ src/main/java/cn/project/spider_1608/download/HttpClientDownload.java
@@ -1,16 +1,5 @@
 package cn.project.spider_1608.download;
 
-import java.io.IOException;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-
 import cn.project.spider_1608.domain.Page;
 import cn.project.spider_1608.utils.PageUtils;
 
@@ -21,6 +10,7 @@ public class HttpClientDownload implements Downloadable {
 		Page page = new Page();
 		String content = PageUtils.getContent(url);
 		page.setContent(content);
+		page.setUrl(url);
 		return page;
 		
 	}
