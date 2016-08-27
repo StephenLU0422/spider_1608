1bb2a42685dc915cb9de9fba1c7a060301847e11
StephenLU0422 <1029252135@qq.com>
2016-08-28 01:16:32 +0800

    Spider with bug！

--- src/main/java/cn/project/spider_1608/utils/PageUtils.java
+++ src/main/java/cn/project/spider_1608/utils/PageUtils.java
@@ -10,8 +10,16 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PageUtils {
+	private static Logger logger = LoggerFactory.getLogger(PageUtils.class);
+	/**
+	 * 根据url获取页面内容
+	 * @param url
+	 * @return
+	 */
 	public static String getContent(String url){
 		String content = "";
 		//获取httpclient
@@ -20,16 +28,18 @@ public class PageUtils {
 		
 		HttpGet request = new HttpGet(url);
 		try {
+			//获取当前时间戳
+			long start_time = System.currentTimeMillis();
 			CloseableHttpResponse response = client.execute(request);
 			//获取页面内容
 			HttpEntity entity = response.getEntity();
 			//打印页面内容
 			content = EntityUtils.toString(entity);
-		} catch (ClientProtocolException e) {
+//			logger.info("页面下载成功:{},消耗时间(/ms):{}",url,System.currentTimeMillis()-start_time);
+		} catch (Exception e) {
+//			logger.info("页面下载失败,url:{}",url);
 			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		} 
 		return content;
 	}
 }
